284e2ba7954b3cf1542aaaffecea763a2af60df5
marvy896 <onyex896@gmail.com>
2023-06-28 09:37:03 +0100

    Email sending works

--- src/contact.js
+++ src/contact.js
@@ -48,10 +48,7 @@ export default function ContactForm(){
 
       // Use emailjs to email contact form data
       await emailjs.send(
-        process.env.REACT_APP_SERVICE_ID,
-        process.env.REACT_APP_TEMPLATE_ID,
-        templateParams,
-        process.env.REACT_APP_USER_ID
+        "service_zi2r5ne", "template_zy55g6j",templateParams,"2yvwASNOXSUZqH6TM"
       );
 
       // Reset contact form fields after submission
